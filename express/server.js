bf67190082c38c926f6e5b993e6abcb8207bdafa
DianaLSL <dianaserranolozano@gmail.com>
2021-08-10 01:37:11 -0500

    comit3

--- express/server.js
+++ express/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 
 const app = express();
